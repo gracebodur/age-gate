94ed5089482b71770e4352a0d39992f2b7760136
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-30 21:59:53 -0400

    Add partial modal styles and  initial modal load functionality

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".nav-menu");
+const modal = document.getElementById("agegate-modal");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
+});
+
+window.addEventListener("load", (e) => {
+  console.log("The page has fully loaded");
+  setTimeout(function () {
+    modal.style.display = "block";
+  }, 2000);
 });
