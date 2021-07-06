ef3d958a38ebe8aef4757db8e1072c7bbf85e785
Marie Grace Bodur <mgracebodur@gmail.com>
2021-07-06 12:48:27 -0400

    Add slideshow transition and interval delay functions

--- app.js
+++ app.js
@@ -20,7 +20,15 @@ const thisYear = todaysDate.getFullYear();
 const thisMonth = todaysDate.getMonth() + 1;
 const thisDay = todaysDate.getDay() + 4;
 const requiredAge = 21;
-const requiredYearOfBirth = 2000;
+
+const items = document.querySelectorAll(".item");
+const controls = document.querySelectorAll(".control");
+const headerItems = document.querySelectorAll(".item-header");
+const descriptionItems = document.querySelectorAll(".item-description");
+const activeDelay = 0.76;
+const interval = 5000;
+
+let current = 0;
 
 let verified;
 
@@ -44,18 +52,18 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   ageVerification();
-  checkInputLength(month, 2, 12)
-  checkInputLength(day, 2, 31)
-  checkInputLength(year, 4, 2025)
+  checkInputLength(month, 2, 12);
+  checkInputLength(day, 2, 31);
+  checkInputLength(year, 4, 2025);
 });
 
 enter.addEventListener("submit", (e) => {
   e.preventDefault();
 
   ageVerification();
-  checkInputLength(month, 2, 12)
-  checkInputLength(day, 2, 31)
-  checkInputLength(year, 4, 2025)
+  checkInputLength(month, 2, 12);
+  checkInputLength(day, 2, 31);
+  checkInputLength(year, 4, 2025);
 });
 
 month.addEventListener("keyup", ageVerification);
@@ -87,9 +95,9 @@ function ageVerification() {
     errorSubtitle.innerHTML = `you must be ${requiredAge} or older to view this site.`;
     bannerBtn.style.display = "none";
     navbar.style.display = "none";
-  } else if (userAge >= requiredAge){
+  } else if (userAge >= requiredAge) {
     setTimeout(() => {
-    modal.style.display = "none";
+      modal.style.display = "none";
     }, 2000);
     setTimeout(() => {
       header.style.display = "initial";
@@ -139,15 +147,14 @@ function checkRequired(inputArr) {
 
 // -------------CHECK INPUT LENGTH-----------------------
 function checkInputLength(input, min, max) {
-  if(input.value.length < min) {
-    showError(input, `${getFieldName} must be at least ${min} characters`)
+  if (input.value.length < min) {
+    showError(input, `${getFieldName} must be at least ${min} characters`);
   } else if (input.value.length > max) {
-    showError(input, `${getFieldName} must be less than ${max} characters`)
+    showError(input, `${getFieldName} must be less than ${max} characters`);
   } else {
-    showValid(input)
+    showValid(input);
   }
 }
-
 
 // -------------ADD ABOOLENA VALUE TO VERIFIED-----------------------
 function successChecker(e) {
@@ -181,5 +188,80 @@ function cookieCheck() {
   }
 }
 
+// -------------SLIDESHOW-------------
+const slider = {
+  init: () => {
+    controls.forEach((control) =>
+      control.addEventListener("click", (e) => {
+        slider.clickedControl(e);
+      })
+    );
+    controls[current].classList.add("active");
+    items[current].classList.add("active");
+  },
+  nextSlide: () => {
+    // Increment current slide and add active class
+    slider.reset();
+    if (current === items.length - 1) current = -1; // Check if current slide is last in array
+    current++;
+    controls[current].classList.add("active");
+    items[current].classList.add("active");
+    slider.transitionDelay(headerItems);
+    slider.transitionDelay(descriptionItems);
+  },
+  clickedControl: (e) => {
+    // Add active class to clicked control and corresponding slide
+    slider.reset();
+    clearInterval(intervalF);
+
+    const control = e.target,
+      dataIndex = Number(control.dataset.index);
+
+    control.classList.add("active");
+    items.forEach((item, index) => {
+      if (index === dataIndex) {
+        // Add active class to corresponding slide
+        item.classList.add("active");
+      }
+    });
+    current = dataIndex; // Update current slide
+    slider.transitionDelay(headerItems);
+    slider.transitionDelay(descriptionItems);
+    intervalF = setInterval(slider.nextSlide, interval); // Fire that bad boi back up
+  },
+  reset: () => {
+    // Remove active classes
+    items.forEach((item) => item.classList.remove("active"));
+    controls.forEach((control) => control.classList.remove("active"));
+  },
+  transitionDelay: (items) => {
+    // Set incrementing css transition-delay for .item-header & .item-description, .vertical-part, b elements
+    let seconds;
+
+    items.forEach((item) => {
+      const children = item.childNodes; // .vertical-part(s)
+      let count = 1,
+        delay;
+
+      item.classList.value === "item-header"
+        ? (seconds = 0.015)
+        : (seconds = 0.007);
+
+      children.forEach((child) => {
+        // iterate through .vertical-part(s) and style b element
+        if (child.classList) {
+          item.parentNode.classList.contains("active")
+            ? (delay = count * seconds + activeDelay)
+            : (delay = count * seconds);
+          child.firstElementChild.style.transitionDelay = `${delay}s`; // b element
+          count++;
+        }
+      });
+    });
+  },
+};
+
+let intervalF = setInterval(slider.nextSlide, interval);
+slider.init();
 
 document.onload = cookieCheck();
